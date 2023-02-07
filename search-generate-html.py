838b31a664d1ee1bac35b4ca944f52b9449755f3
Aniket Shaw <85289910+aniketshaw29@users.noreply.github.com>
2023-02-07 19:36:32 +0530

    first commit

---
+++ search-generate-html.py
@@ -0,0 +1,22 @@
+import requests
+
+def google_search(query):
+    query = query.replace(" ", "+")
+    url = f"https://www.google.com/search?q={query}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+search_query = "Python programming language"
+result = google_search(search_query)
+
+if result:
+    html_file = open("index.html", "w")
+    html_file.write(result)
+    html_file.close()
+
+else:
+    print("Search failed")
---
+++ search-generateLinks.py
@@ -0,0 +1,6 @@
+from googlesearch import search
+
+query = "Java"
+
+for result in search(query, num=10, stop=10, pause=2):
+    print(result)
